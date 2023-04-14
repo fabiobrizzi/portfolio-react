20ca4cbd82814f47cc4aaf6f5d5644cb202fbc0b
FabrizioEstrella <efabrizio@yulcom.ca>
2023-04-14 00:00:49 -0400

    Navbar Completer

--- src/App.js
+++ src/App.js
@@ -18,7 +18,8 @@ const App = () => {
       <Services />
       <Work />
       <Contact />
-      {/* <div className='h-[4000px]'></div> */}
+      {/* This div is for background */}  
+      <div className='h-[4000px]'></div>
     </div>
   );
 };
